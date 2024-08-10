fb607f7d328e3eaa1c4eb306fed5335d1a81ccb3
Martin Reisenhofer <martin@reisenhofer.biz>
2024-08-10 15:17:25 +0200

    [IMP] l10n_at_pos: addedd signed state.

--- l10n_at_pos/static/src/overrides/models.js
+++ l10n_at_pos/static/src/overrides/models.js
@@ -8,8 +8,25 @@ patch(Order.prototype, {
     setup() {
         super.setup(...arguments);
         if (this.pos.config.asign_enabled) {
-            this.asign = this.asign || 'u';
+            this.asign_state = this.asign_state || 'u';
             this.save_to_db();
         }
+    },
+
+    export_as_JSON() {
+        var json = super.export_as_JSON();
+        if (this.asign_state !== undefined) {
+            json.asign_state = this.asign_state;
+        }
+        return json;
+    },
+
+    export_for_printing() {
+        debugger;
+        var json = super.export_for_printing();
+        if (this.asign_state !== undefined) {
+            json.asign_state = this.asign_state;
+        }
+        return json;
     }
 });
